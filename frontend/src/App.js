0273731f1c9c6b506610aedb84a48f3f9f24767d
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2023-09-16 17:34:04 +0545

    Fixed directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@
 // import Navbar from "./components/templates/Navbar";
 // import LandingPage from "./pages/LandingPage";
 
-import Dashboard from "./components/dashboard/Dashboard";
+// import Dashboard from "./components/dashboard/Dashboard";
+import UserMain from "./pages/users/UserMain";
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar />
       <LandingPage /> */}
-      <Dashboard />
+      <UserMain />
       {/* <Footer /> */}
     </div>
   );
--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -6,9 +6,9 @@ const Dashboard = () => {
     return (
         <div className=''>
             <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-dash" role="tabpanel" aria-labelledby="pills-dash-tab" tabindex="0">
-                    Hi i am here
-                </div>
+                    <div class="tab-pane fade show active" id="pills-dash" role="tabpanel" aria-labelledby="pills-dash-tab" tabindex="0">
+                        Hi i am here
+                    </div>
                 <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
                     Profile
                 </div>
